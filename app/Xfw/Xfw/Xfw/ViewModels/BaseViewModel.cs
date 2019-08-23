8df4169e36729165c78b7794125167ef69e0c4c4
William <Rodriguez>
2019-08-23 20:47:11 -0300

    initial commit

---
+++ app/Xfw/Xfw/Xfw/ViewModels/BaseViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xfw.ViewModels
+{
+    public abstract class BaseViewModel : BindableObject
+    {
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set { isBusy = value; OnPropertyChanged(); }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public async Task ExecuteBusyAction(Func<Task> theBuysAction)
+        {
+            if (IsBusy)
+                return;
+          
+            try
+            {
+                IsBusy = true;
+
+                await theBuysAction();
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                System.Diagnostics.Debug.WriteLine(ex);
+#endif
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        protected virtual Task Initialize()
+            => Task.CompletedTask;
+
+    }
+}
